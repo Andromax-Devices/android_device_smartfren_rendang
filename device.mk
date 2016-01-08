088b5a5675fcd49cc22be01a182045e22e66f656
LuK1337 <priv.luk@gmail.com>
2016-01-08 19:37:41 +0100

    rendang: Implement CameraWrapper
    * Setting zsl causes HDR to be broken
    
    Change-Id: I5a2c7f483a132dd15be02a9c8fdaf9934a900002
    
    Fix typo

--- device.mk
+++ device.mk
@@ -190,6 +190,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
+    camera.msm8916 \
     Snap
 
 # Screen Recorder
