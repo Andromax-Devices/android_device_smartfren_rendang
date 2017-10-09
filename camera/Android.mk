271de13cc23166d25a7ed3a734f1c943b346321e
Arne Coucheron <arco68@gmail.com>
2017-10-10 00:18:36 +0200

    rendang: Move camera wrapper to vendor
    Change-Id: I6752fd7c9b918522e588fa830669a142f3fd7290

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,10 +9,11 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libgui
+    libhardware liblog libcamera_client libutils libcutils libgui libsensor
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
