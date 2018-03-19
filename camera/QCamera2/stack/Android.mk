dcad8d7bc917a879d086dccb7bf35b76375c6bac
Michael Bestas <mkbestas@lineageos.org>
2018-03-19 04:26:16 +0200

    QCamera2: Cleanup makefiles
    Change-Id: I96bd8a79e74e6bed8bcc8d39010411de26ba0fcd

--- camera/QCamera2/stack/Android.mk
+++ camera/QCamera2/stack/Android.mk
@@ -1,4 +1,1 @@
-LOCAL_PATH:= $(call my-dir)
-include $(LOCAL_PATH)/mm-camera-interface/Android.mk
-include $(LOCAL_PATH)/mm-jpeg-interface/Android.mk
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+include $(call all-subdir-makefiles)
