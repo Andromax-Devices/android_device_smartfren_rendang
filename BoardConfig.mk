baba6f5670d66f0c54e675c18908ecc5676c2de9
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    rendang: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,14 +39,6 @@ TARGET_USES_AOSP := true
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-
 # Display
 TARGET_SCREEN_DENSITY := 240
 
