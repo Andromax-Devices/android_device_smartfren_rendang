8e8d72924cd30b14d08eb332cb9a726827145e19
dodyirawan85 <johntheryjt@gmail.com>
2020-08-02 11:30:28 +0700

    rendang: rootdir: Set min gpu freq to 400Mhz
    * For better UX but maybe worse battery life

--- rootdir/etc/init.boot_boost.sh
+++ rootdir/etc/init.boot_boost.sh
@@ -5,10 +5,10 @@
 
 echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "performance" > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/governor
-echo 400000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
+echo 465000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
 
 sleep 40
 
 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "msm-adreno-tz" > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/governor
-echo 200000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
+echo 400000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
